2cfb3e11f3e6ef065f40aecebdf4912e81791183
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-09 21:48:16 +0100

    examples

---
+++ examples.py
@@ -0,0 +1,26 @@
+
+x = 500
+
+if x > 200 :
+    print('x > 200')
+
+if x > 100 :
+    print('x > 100')
+
+if x > 50 :
+    print('x > 50')
+
+
+x = 5
+
+if x > 200 :
+    print('x > 200')
+
+elif x > 100 :
+    print('x > 100')
+
+elif x > 50 :
+    print('x > 50')
+
+else:
+    print(x)
