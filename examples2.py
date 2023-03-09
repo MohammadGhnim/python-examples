5362219ca1dd0da1fc21f748c2e43a833508c52c
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-09 22:31:57 +0100

    add-examples

---
+++ examples2.py
@@ -0,0 +1,64 @@
+x = 22
+if x >= 0 and x <= 20:
+    print('True')
+
+
+print('------------------------')
+
+y = 2
+if y == 1 or y == 2 or y == 3:
+    print('valid')
+
+print('------------------------')
+
+a = 2
+b = 4
+if a > b: print('a is greater than b')
+
+print('------------------------')
+
+name = 'Ali'
+job = 'Developer'
+
+if name == 'Ali':
+    print('Name: Ali')
+if job == 'Developer':
+    print('Nice')
+
+print('-------------------------')
+
+a = 33
+b = 33
+
+if b > a:
+    print('b is greater than a')
+elif a == b:
+    print('a and b are equal')
+
+print('-------------------------')
+
+sex = 'Female'
+age = 30
+
+if sex == 'male':
+    print('Gender: male')
+if age <= 31:
+        print('he is a young boy')
+elif sex == 'Female':
+    print('Gender: female')
+if age >= 21:
+        print('she is a young girl')
+
+print('--------------------------')
+
+sex = 'Female'
+age = 30
+
+if sex == 'male':
+    if age <= 31:
+        print('he is a young boy')
+    elif sex == 'Female':
+        print('Gender: female')
+if age >= 21:
+        print('she is a young girl')
+
