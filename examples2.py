44edb1f54ce8bc425b0fc384a233fb879528e163
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-11 22:31:47 +0100

    more-loops-example

--- examples2.py
+++ examples2.py
@@ -72,3 +72,100 @@ if num >= 0:
         print('Positiv number')
 else:
     print('Negativ number')
+
+print('----------------------------')
+
+numbers = [1, 2, 3, 4, 5]
+for num in numbers:
+    print(num)
+
+print('----------------------------')
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+print('----------------------------')
+
+days = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+for day in days:
+    print(day)
+
+print('----------------------------')
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+
+print('----------------------------')
+
+days = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+for day in days:
+    print(day)
+    if day == "Monday":
+        break
+
+
+print('----------------------------')
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
+
+
+print('----------------------------')
+
+days = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+for day in days:
+    if day == "Monday":
+        continue
+    print(day)
+
+
+print('----------------------------')
+
+numbers = [1, 2, 3, 4, 5]
+for x in numbers:
+    print(x)
+else:
+    print("Finally finished")
+
+
+print('----------------------------')
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+for x in adj:
+    for y in fruits:
+        print(x, y)
+
+print('----------------------------')
+
+i = 0
+while i < 6:
+    print(i)
+    i += 1
+
+print('----------------------------')
+
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+
+print('----------------------------')
+
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
