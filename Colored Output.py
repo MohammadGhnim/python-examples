a31e2ca101f19af99ea6270e040d37786a7d4f54
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-10-13 23:48:00 +0200

    add new file colored output

---
+++ Colored Output.py
@@ -0,0 +1,7 @@
+from colorama import Fore
+
+print(Fore.RED + "Hello world")
+print(Fore.BLUE + "Hello world")
+print(Fore.GREEN + "Hello world")
+print(Fore.YELLO + "Hello world")
+print(Fore.CYAN + "Thank you!")
